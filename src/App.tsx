d15184572b6f80def5bff8cf59d02c0f10b63031
tamago3keran <yamataku3831@gmail.com>
2021-11-08 23:49:47 +0900

    3d modeling portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Container, Grid, Paper } from "@mui/material";
+import { DenseAppBar } from "./DenseAppBar";
+import { ModelingCard } from "./ModelingCard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <DenseAppBar />
+      <Container maxWidth="xl">
+        <Grid container sx={{ py: 4 }} spacing={4}>
+          <Grid item lg={2} md={3} sm={4} xs={6}>
+            <Paper style={{ height: 200 }}>
+              <ModelingCard name={"notebook"} />
+            </Paper>
+          </Grid>
+          <Grid item lg={2} md={3} sm={4} xs={6}>
+            <Paper style={{ height: 200 }}>
+              <ModelingCard name={"house"} />
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
+    </>
   );
 }
 
---
+++ src/DenseAppBar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+
+export const DenseAppBar: React.FC = () => {
+  return (
+    <Box component="div" sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <Typography variant="h6" color="inherit" component="div">
+            3D Modeling Portfolio
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
