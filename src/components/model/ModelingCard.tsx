fb0e02221d08c4c1158c01b5465d57807858cfc8
tamago3keran <yamataku3831@gmail.com>
2021-11-23 23:40:36 +0900

    Slide-in ModelingCard

--- src/components/model/ModelingCard.tsx
+++ src/components/model/ModelingCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Paper } from "@mui/material";
+import { Paper, Slide } from "@mui/material";
 import { styled } from "@mui/system";
 import { ModelingContainer } from "./ModelingContainer";
 
@@ -11,10 +11,12 @@ const Container = styled(Paper)({
   height: 200,
 });
 
-export const ModelingCard: React.FC<Props> = (props) => {
+export const ModelingCard: React.FC<Props> = ({ name }) => {
   return (
-    <Container>
-      <ModelingContainer name={props.name} />
-    </Container>
+    <Slide direction={"up"} in={true} timeout={400}>
+      <Container>
+        <ModelingContainer name={name} />
+      </Container>
+    </Slide>
   );
 };
